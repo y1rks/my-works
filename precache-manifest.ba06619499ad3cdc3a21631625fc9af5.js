55921ec85bc1df106cbb308e652adaf1853020bf
y1rks <yu.martian@gmail.com>
2020-04-25 14:51:42 +0900

    Updates

---
+++ precache-manifest.ba06619499ad3cdc3a21631625fc9af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0298da0e224d74c36d33402951385b",
+    "url": "/my-works/index.html"
+  },
+  {
+    "revision": "87150580287339fa7e53",
+    "url": "/my-works/static/css/main.150bb923.chunk.css"
+  },
+  {
+    "revision": "90a25409a994b942d2c8",
+    "url": "/my-works/static/js/2.9dbe3bef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-works/static/js/2.9dbe3bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87150580287339fa7e53",
+    "url": "/my-works/static/js/main.15fd157d.chunk.js"
+  },
+  {
+    "revision": "8b791f02e9232102d677",
+    "url": "/my-works/static/js/runtime-main.896f8684.js"
+  }
+]);
